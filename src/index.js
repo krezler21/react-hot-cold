ab448b5fea72f4298398e6d087b86a2edeb5284f
krezler21 <krezler21@gmail.com>
2024-06-10 21:55:23 +0200

    fix: 2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import "./index.css";
-
+// komentarz
 import Game from "./components/Game";
 
 render(<Game />, document.getElementById("root"));
